e5bef673ba6d3dfb6ce446fd3c70eea0f43e3f32
luanpiresbresolla <luanpires34@gmail.com>
2020-05-10 20:35:04 -0500

    Finalizando parte dos usuarios da aplicaçao

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -4,12 +4,14 @@ import appointmentsRouter from '@modules/appointments/infra/http/routes/appointm
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import passwordRouter from '@modules/users/infra/http/routes/password.routes';
+import profileRouter from '@modules/users/infra/http/routes/profile.routes';
 
 const routes = Router();
 
 routes.use('/sessions', sessionsRouter);
 routes.use('/users', usersRouter);
 routes.use('/password', passwordRouter);
+routes.use('/profile', profileRouter);
 routes.use('/appointments', appointmentsRouter);
 
 export default routes;
